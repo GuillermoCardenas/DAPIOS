ca663e2cd778ef1a6c7218568b0fcb14124f6034
GuillermoCardenas <59988675+GuillermoCardenas@users.noreply.github.com>
2020-03-17 04:30:11 -0500

    Add files via upload

---
+++ Actividad10/Actividad10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad10
+//
+//  Created by user166251 on 3/17/20.
+//  Copyright © 2020 user166251. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
